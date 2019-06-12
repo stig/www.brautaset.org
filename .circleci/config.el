353b013332530447e5de3f487329212430fff595
Stig Brautaset <stig@brautaset.org>
2019-06-12 15:02:31 +0100

    Split publishing config for pages and articles
    Blog posts (articles) get a slightly shorter version of the home/up menu.

--- .circleci/config.el
+++ .circleci/config.el
@@ -1,12 +1,25 @@
+(setq org-html-postamble "<p>Copyright &copy; Stig Brautaset. <a accesskey=\"l\" href=\"https://creativecommons.org/licenses/by-sa/4.0/\">Licence</a></p>
+")
+
 (defun sb/org-html-format-drawer (name content)
   (concat "<div class=\"drawer " (downcase name) "\">\n"
 	  "<h6>" (capitalize name) "</h6>\n"
 	  content
 	  "\n</div>"))
 
+(defun sb/html-head (prefix)
+  (format "
+       <link rel=\"stylesheet\" type=\"text/css\" href=\"%s/etc/main.css\" />
+       <link rel=\"icon\" type=\"image/png\" href=\"%s/etc/icon.png\" />
+       <link rel=\"alternative\" type=\"application/rss+xml\"
+	     href=\"https://www.brautaset.org/index.xml\"
+	     title=\"Stig's Soapbox RSS Feed\" />
+       <meta name=\"referrer\" content=\"same-origin\">
+" prefix prefix))
+
 (setq org-publish-project-alist
-      '(("www"
-	 :components ("www-pages" "www-static" "www-rss"))
+      `(("www"
+	 :components ("www-pages" "www-articles" "www-static" "www-rss"))
 
 	("www-static"
 	 :base-directory "~/blog"
@@ -20,7 +33,6 @@
 	 :base-directory "~/blog"
 	 :publishing-directory "~/public_html"
 	 :publishing-function org-html-publish-to-html
-	 :recursive t
 	 :section-numbers nil
 	 :time-stamp-file nil
 	 :with-toc nil
@@ -36,27 +48,50 @@
        <div id=\"org-div-home-and-up\">
 	 <nav>
 	   <ul>
-	     <li><a accesskey=\"H\" href=\"%s\"> Home </a> (<a href=\"/index.xml\">RSS</a>)</li>
-	     <li><a accesskey=\"p\" href=\"/publications.html\"> Publications </a></li>
+	     <li><a accesskey=\"H\" href=\"%s\"> Home </a></li>
 	     <li><a accesskey=\"A\" href=\"/about.html\"> About </a></li>
-	     <li>Licence: <a accesskey=\"l\" href=\"https://creativecommons.org/licenses/by-sa/4.0/\">CC BY-SA 4.0</a></li>
+	     <li><a accesskey=\"p\" href=\"/publications.html\"> Publications </a></li>
 	   </ul>
 	 </nav>
        </div>"
-	 :html-head "
-       <link rel=\"stylesheet\" type=\"text/css\" href=\"/etc/main.css\" />
-       <link rel=\"icon\" type=\"image/png\" href=\"/etc/icon.png\" />
-       <link rel=\"alternative\" type=\"application/rss+xml\"
-	     href=\"https://www.brautaset.org/index.xml\"
-	     title=\"Stig's Soapbox RSS Feed\" />
-       <meta name=\"referrer\" content=\"same-origin\">
-     "
+	 :html-head ,(sb/html-head "")
 
 	 :html-head-include-default-style nil
 	 :html-head-include-scripts nil
 
-	 :html-preamble nil
-	 :html-postamble-format auto
+	 :html-metadata-timestamp-format "%e %B %Y")
+
+	("www-articles"
+	 :exclude ",.*"
+	 :base-directory "~/blog/articles"
+	 :publishing-directory "~/public_html/articles"
+	 :publishing-function org-html-publish-to-html
+	 :recursive t
+	 :section-numbers nil
+	 :time-stamp-file nil
+	 :with-toc nil
+	 :with-drawers t
+	 :html-format-drawer-function sb/org-html-format-drawer
+
+	 :html-html5-fancy t
+	 :html-doctype "html5"
+	 :html-footnotes-section "<div id=\"footnotes\"><!--%s-->%s</div>"
+	 :html-link-up "../../"
+	 :html-link-home "../../"
+	 :html-home/up-format "
+       <div id=\"org-div-home-and-up\">
+	 <nav>
+	   <ul>
+	     <li><a accesskey=\"H\" href=\"%s\"> Home </a></li>
+             <li><a accesskey=\"A\" href=\"%s/about.html\"> About </a></li>
+	   </ul>
+	 </nav>
+       </div>"
+	 :html-head ,(sb/html-head "../..")
+
+	 :html-head-include-default-style nil
+	 :html-head-include-scripts nil
+
 	 :html-metadata-timestamp-format "%e %B %Y")
 
 	("www-rss"
@@ -71,4 +106,3 @@
 	 :exclude ".*"              ;; To exclude all files...
 	 :include ("index.org")     ;; ... except index.org.
 	 :table-of-contents nil)))
-
