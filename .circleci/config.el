0b5fbf4bc5711a93e8cff282e82bccb812142389
Stig Brautaset <stig@brautaset.org>
2019-06-14 09:15:55 +0100

    Extract deps to a separate file & install org *first*

--- .circleci/config.el
+++ .circleci/config.el
@@ -110,27 +110,3 @@
 	 :exclude ".*"              ;; To exclude all files...
 	 :include ("index.org")     ;; ... except index.org.
 	 :table-of-contents nil)))
-
-(package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-(package-refresh-contents)
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-
-(use-package htmlize :ensure t)
-
-(use-package clojure-mode :ensure t)
-(use-package clojure-mode-extra-font-locking :ensure t)
-
-(use-package scala-mode :ensure t)
-
-(use-package org
-  :ensure org-plus-contrib
-  :config
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((ditaa . t))))
