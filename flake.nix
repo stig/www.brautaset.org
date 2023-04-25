7a9479d5310edb09635acf8221c01cda22f69ac5
Stig Brautaset <stig@brautaset.org>
2023-04-25 11:49:27 +0100

    Use standard nixpkgs rather than my custom one
    My PR to add ox-rss to nixpkgs was merged, so I no longer need to
    refer to my custom nixpkgs repo.

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
   description = "Blog environment";
 
-  inputs.ox-rss.url = "github:stig/nixpkgs/ox-rss";
-
-  outputs = { self, ox-rss }:
+  outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "aarch64-darwin" "x86_64-linux" ];
-      forAllSystems = ox-rss.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import ox-rss { inherit system; });
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in {
       packages = forAllSystems (system:
         let
