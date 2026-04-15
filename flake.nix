5d4cd7f71aee96499f2fedee2bf80ba3e545222d
Stig Brautaset <stig@brautaset.org>
2026-04-16 00:22:05 +0100

    Support x86_64-darwin
    I'm back on an old Mac as my personal machine.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "aarch64-darwin" "x86_64-linux" ];
+      supportedSystems = [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in {
